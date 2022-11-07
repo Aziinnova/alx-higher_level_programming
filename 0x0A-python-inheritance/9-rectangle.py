5137ae47b9ca9a8a6c29442cfedddcfd85c35e7e
Aziinnova p <natanaba8@gmail.com>
2022-11-07 19:47:21 +0300

    p

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-# 9-rectangle.py
-"""Defines class Rectangle that inherit BaseGeometry.
+
+"""Defines a class Rectangle that inherits
+from the BaseGeometry class.
+
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-        """Rep a rectangle."""
+    """A class Rectangle that inherits
+    from the BaseGeometry
 
-            def __init__(self, width, height):
-                        """instatiate.
-                                Args:
-                                            width (int): width
-                                                        height (int): height
-                                                                """
-                                                                        super().integer_validator("width", width)
-                                                                                self.__width = width
-                                                                                        super().integer_validator("height", height)
-                                                                                                self.__height = height
+    """
 
-                                                                                                    def area(self):
-                                                                                                                """Return area
-                                                                                                                    """
-                                                                                                                            return self.__width * self.__height
+    def __init__(self, width, height):
+        """Instantiation of width and height"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
 
-                                                                                                                            def __str__(self):
-                                                                                                                                        st = "[" + str(self.__class__.__name__) + "] "
-                                                                                                                                                st += str(self.__width) + "/" + str(self.__height)
-                                                                                                                                                        return st
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str()
+        representation of a Rectangle.
+
+        """
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
