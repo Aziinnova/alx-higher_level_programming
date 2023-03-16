132af810f21e0036ab25ab49a2fd49bd266e1533
Aziinnova <aziazinnova@gmil.com>
2023-03-16 14:23:04 +0300

    py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-    language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+ language that combines remarkable power with very clear syntax"
+print(str.split()[6] + " " + str.split()[7] + " " + str.split()[8] + ",\n")
+
